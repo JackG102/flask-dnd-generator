e7d3de6758290c4d1958be881753fc50d3193681
Jack Garratt <jack.garratt@icfnext.com>
2024-02-21 06:44:49 -0500

    Really starting to get a good POC for npc page

--- app/dnd_generator/static/fetch-json.js
+++ app/dnd_generator/static/fetch-json.js
@@ -9,8 +9,17 @@ const fetchJson = endpoint => {
   });
 }
 
+// TODO write functions that render the object specific to each item
+// Pass them into this function as arguments 
 const printJsonData = data => {
-  document.getElementById("target_json_container").textContent = JSON.stringify(data, undefined, 2);
+  const parsed_object = JSON.parse(JSON.stringify(data, undefined, 2));
+  console.log(parsed_object);
+  document.getElementById("target_json_container").innerHTML = "Converting JSON to HTML <br><br>" + 
+  "Name: " + parsed_object.Name + 
+  "<br>Race: " + parsed_object.Race +
+  "<br>Gender: " + parsed_object.Gender +
+  "<br>Vocation: " + parsed_object.Vocation +
+  "<br>Alignment: " + parsed_object.Alignment;
 }
 
 
