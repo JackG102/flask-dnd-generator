e2bcf7ab1cc63b958895df89f3416faf8bf115e9
Jack Garratt <jack.garratt@icfnext.com>
2024-02-21 06:26:48 -0500

    Made POC npc generator page with JS functions, still ugly printing out json

--- app/dnd_generator/static/fetch-json.js
+++ app/dnd_generator/static/fetch-json.js
@@ -1,1 +1,16 @@
-console.log('running')
+const fetchJson = endpoint => {
+  fetch(endpoint)
+  .then(request => {
+    return request.json();
+  })
+  .then(data => {
+    console.log(data);
+    printJsonData(data);
+  });
+}
+
+const printJsonData = data => {
+  document.getElementById("target_json_container").textContent = JSON.stringify(data, undefined, 2);
+}
+
+
