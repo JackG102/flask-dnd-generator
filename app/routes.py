57711a5084ce83d1dc8c39dcf06d62e39ca56a1b
Jack Garratt <jack.garratt@icfnext.com>
2024-02-21 06:33:34 -0500

    Changed some routes

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,6 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
   user = {'username': 'Jack'}
   return render_template('index.html', title='Home', user=user)
